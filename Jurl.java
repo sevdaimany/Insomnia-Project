68c517dffd0c93003eb44534b6ab29a2e67d27c7
sevda <imanysevda@gmail.com>
2020-06-02 10:47:06 +0430

    Add show requests info in list

--- Jurl.java
+++ Jurl.java
@@ -1,6 +1,10 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Random;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class Jurl {
 
@@ -15,17 +19,34 @@ public class Jurl {
 
         String name = Integer.toString(new Random().nextInt(1000000000)) + ".txt";
 
-        File saveRequestFile = new File("./requests/" + "sevda" + "/" + name);
+        File saveRequestFile = new File("./requests/" + nameDirectory + "/" + name);
         saveRequestFile.createNewFile();
         FileSave saveRequest = new FileSave();
         saveRequest.save(request, saveRequestFile);
     }
 
-    public void showList(){
+    public void showList() {
         File Directory = new File("./requests");
         File[] filesList = Directory.listFiles();
-        for(int i = 0 ; i < filesList.length ; i++){
-            System.out.println((i+1)+": "+filesList[i].getName());
+        for (int i = 0; i < filesList.length; i++) {
+            System.out.println((i + 1) + ": " + filesList[i].getName());
+        }
+    }
+
+    public void showRequestsInList(String nameDirectory) throws Exception {
+        File nameFile = new File("./requests/" + nameDirectory);
+        if (nameFile.exists()) {
+            ObjectInputStream in =null;
+            File[] requestList = nameFile.listFiles();
+            for (int i = 0; i < requestList.length; i++) {
+
+                in = new ObjectInputStream(new FileInputStream(requestList[i]));
+                Request request = (Request)in.readObject();
+                System.out.println((i + 1) + ". url: " +request.getUrl()+" | method: "+request.getMethod() + ((request.getRequestsHeaders().size() == 0)?"":(" | headers : "+ request.getRequestsHeaders().toString()) ) +((request.getBody().size() == 0)?"":(" | formData : "+ request.getBody().toString()) ) ); 
+
+            }
+            in .close();
+
         }
     }
 
